b5fec1a6a8d455bd829e4edce030541d8d36a3b1
ask.websync <ask.websync@gmail.com>
2023-12-13 20:41:04 +0700

    first deploy

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './assets/css/app.css';
-import 'tw-elements';
 import BackToTop from 'vue-backtotop';
 
 const feather = require('feather-icons');
